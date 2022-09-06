9da0fdb31be2cf11baec61a10930e313021f1f20
João Gabriel <joao951biel@gmail.com>
2022-09-06 20:44:28 -0300

    função add no carrinho

--- Projeto/js/script.js
+++ Projeto/js/script.js
@@ -1,10 +1,11 @@
 let xhttp = new XMLHttpRequest();
+let lsProduto = [];
 
 function buscarProduto(){
   xhttp.open("GET","https://pascoa-chiquinha.herokuapp.com/produto/");
   xhttp.send();
   xhttp.onload = function(){
-    let lsProduto = this.response;
+    lsProduto = this.response;
     lsProduto = JSON.parse(lsProduto);
     montarListaProdutosHtml(lsProduto);
     
@@ -13,17 +14,33 @@ function buscarProduto(){
 
 function montarListaProdutosHtml(lsProduto){
   let listaProduto = "";
+  let i = 0;
   for (produto of lsProduto) {
     listaProduto += `
     <div class="ls-produto">
     <div class="produto">
-       <img src="https://img.elo7.com.br/product/original/17B1547/camisa-sublimacao-poliester.jpg" alt="">
+       <img src="${produto.imagem} alt="">
        <p class="nome">${produto.nome}</p>
-       <p class="descricao">Descrição</p>
-       <p class="valor">R$: 100 <span class="btadd"><i class="material-icons">&#xe8cc;</i></span></p></span></p>
+       <p class="descricao">${produto.descricao}</p>
+       <p class="valor">R$: ${produto.valor.toFixed(2)} <span class="btadd"><i class="material-icons" onclick="addProdutoCarrinho(${i})">&#xe8cc;</i></span></p></span></p>
     </div> 
    </div>`;
+   i++;
   }
   document.getElementById("listaProduto").innerHTML = listaProduto;
+  document.getElementById("listaProduto").style.display = '';
 }
+
+function addProdutoCarrinho(i){
+  let produto = lsProduto[i];
+  if(produto.carrinho == undefined || produto.carrinho == false){
+  produto.carrinho = true;
+  document.getElementsByClassName("material-icons")[i].style.color = "#e66b6b";
+  }else{
+    produto.carrinho = false;
+    document.getElementsByClassName("material-icons")[i].style.color = "#0000007d";
+  }
+
+}
+
 buscarProduto();
