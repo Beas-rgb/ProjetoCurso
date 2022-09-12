cb74c831ade9cbcd787b99d2178a6a2abe7b4354
João Gabriel <joao951biel@gmail.com>
2022-09-12 20:06:39 -0300

    codigo projeto

--- Projeto/js/script.js
+++ Projeto/js/script.js
@@ -55,7 +55,15 @@ function addProdutoCarrinho(i){
   localStorage.setItem("listaProdutoLocalStorage",JSON.stringify(lsProduto));
 }
 
+let verCarrinho = false;
+
 function verListaProdutoSelecionado(){
+  if(verCarrinho){
+    buscarProduto();
+    verCarrinho = false;
+  }else{
+    verCarrinho = true;
+  }
   let listaProduto = "";
   document.getElementById("listaProduto").innerHTML = "";
   let i = 0;
@@ -75,7 +83,7 @@ function verListaProdutoSelecionado(){
          <span class="btMais" onclick="add(1,${i},${j})">+</span>
           <span class="btMenos" onclick="add(-1,${i},${j})">-</span>
        </span>
-       
+
          <span class="quantidade">${produto.quantidade}</span>
 
     </div> 
@@ -85,11 +93,36 @@ function verListaProdutoSelecionado(){
   j++;
   }
   document.getElementById("listaProduto").innerHTML = listaProduto;
+  document.getElementById("formulario").style.display = "grid";
 }
 
 function add(qt, i, j){
   lsProduto[j].quantidade += qt;
+  if(lsProduto[j].quantidade == 0){
+    lsProduto[j].quantidade = 1;
+    return;
+  }
   document.getElementsByClassName("quantidade")[i].innerHTML = lsProduto[j].quantidade;
+}
+
+function enviarPedido(){
+  let pedido = '';
+  let total = 0;
+  for (produto of lsProduto) {
+    if(produto.carrinho){
+      let subtotal = produto.quantidade * produto.valor;
+      pedido += `${produto.nome} ${produto.quantidade} x ${produto.valor} = ${subtotal}`;
+      total += subtotal;
+    }
+  }
+  let nome = document.getElementById('nome').value;
+  let endereco = document.getElementById('endereco').value;
+  let msg = `Olá gostaria de fazer o seguinte pedido:\n ${pedido} \nTotal: ${total} \nNome: ${nome} \nEndereço:${endereco}`;
+  msg = encodeURI(msg);
+  let fone = '5561985314554';
+  link = `https://api.whatsapp.com/send?phone=${fone}&text=${msg}`;
+  window.open(link, '_blank');
+
 }
 
 buscarProduto();
